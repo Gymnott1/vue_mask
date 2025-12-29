0f8f68e4c62c1e92f312130d0b8965158d0766f3
Linus Wabwire <gymnottwc@gmail.com>
2025-12-29 20:59:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1 class="bg-red-500 text-white p-5">Hello Tailwind CSS!</h1>
+  <p class="mt-4 text-gray-700">
+    This is a simple Vue.js application styled with Tailwind CSS.
+  </p>
+</template>
+
+<style scoped></style>
